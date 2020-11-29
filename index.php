16c6909528217a934952a02d4b1c20b0afe0b901
wangdadaya <1287980628@qq.com>
2020-11-29 17:45:00 +0800

    modify

---
+++ index.php
@@ -0,0 +1,9 @@
+<div style="background:url('https://img.tupianzj.com/uploads/allimg/202003/9999/c3e2092e14.jpg') no-repeat; position: relative;background-position: center;width: 100%;
+			height: 100%;
+			background-size: 100% 100%;">
+<?php 
+    echo"hello world<br>";
+    echo"<a href='video.html'><input type='button', value='点我播放音乐'></a>";
+    echo"<a href='/ppp/index.html'><input type='button', value='进入我的小世界'></a>";
+    echo("<img src='http://img.baidu.com/img/logo-zhidao.gif' />");
+?>
